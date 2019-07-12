7aaa38027842a21fba1eec2a4b72b72ab6710f2b
enurgaleeva <enurgaleeva@braintch.com>
2019-07-12 16:56:42 +0500

    fixed multiple links

--- test_product_page.py
+++ test_product_page.py
@@ -4,6 +4,8 @@ from pages.basket_page import BasketPage
 import pytest
 import time
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 								])
@@ -16,20 +18,17 @@ def test_guest_can_add_product_to_cart(browser, link):
 	page.should_be_message_basket_price()
 
 def test_guest_cant_see_success_message(browser):
-	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 	page = ProductPage(browser, link)
 	page.open()
 	page.should_not_be_success_message()
 
 def test_guest_should_see_login_link_on_product_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = ProductPage(browser, link)
 	page.open()
 	page.should_be_login_link()
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = ProductPage(browser, link)
 	page.open()
 	page.go_to_login_page()
@@ -38,7 +37,6 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 	page = ProductPage(browser, link)
 	page.open()
 	page.go_to_basket_page()
@@ -48,6 +46,8 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
 
 @pytest.mark.registered_user
 class TestUserAddToCartFromProductPage():
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
 	@pytest.fixture(scope="function", autouse=True)
 	def setup(self, browser):
 		register_link = "http://selenium1py.pythonanywhere.com/accounts/login/"
@@ -59,18 +59,14 @@ class TestUserAddToCartFromProductPage():
 		register_page.should_be_authorized_user()
 
 	@pytest.mark.need_review
-	@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-								])
-	def test_user_can_add_product_to_cart(self, browser, link):
+	def test_user_can_add_product_to_cart(self, browser):
 		page = ProductPage(browser, link)
 		page.open()
 		page.add_to_basket()
-		page.solve_quiz_and_get_code()
 		page.should_be_message_basket_item()
 		page.should_be_message_basket_price()
 
 	def test_user_cant_see_success_message(self, browser):
-		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 		page = ProductPage(browser, link)
 		page.open()
 		page.should_not_be_success_message()
